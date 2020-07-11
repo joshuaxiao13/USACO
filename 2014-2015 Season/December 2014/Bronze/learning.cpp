159a108fffa2b502c5ecf483cbc6d34f059eed66
Joshua Xiao <66401883+joshuaxiao13@users.noreply.github.com>
2020-07-10 21:26:52 -0400

    Update learning.cpp

--- 2014-2015 Season/December 2014/Bronze/learning.cpp
+++ 2014-2015 Season/December 2014/Bronze/learning.cpp
@@ -25,8 +25,8 @@ int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
     
-    //ofstream cout ("learning.out");
-    //ifstream cin ("learning.in");
+    ofstream cout ("learning.out");
+    ifstream cin ("learning.in");
     
     int n,a,b;
     cin>>n>>a>>b;
