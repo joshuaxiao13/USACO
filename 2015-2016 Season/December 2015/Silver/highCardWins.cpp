133b6bf5e73f968248ddd431b6e0d09002e7e3e8
Joshua Xiao <66401883+joshuaxiao13@users.noreply.github.com>
2021-02-20 16:08:06 -0500

    Update highCardWins.cpp

--- 2015-2016 Season/December 2015/Silver/highCardWins.cpp
+++ 2015-2016 Season/December 2015/Silver/highCardWins.cpp
@@ -2,7 +2,7 @@
 // if not possible to win current round, greedily play smallest card in deck
 // use DSU (Disjoint Set Union) to efficiently find smallest card remaining higher than Elsie's card
 // Ex. if Elsie plays a card with value x, the card Bessie should play is find(x)
-// Remember to handle the case where find(x+1) > 2*N, this means that Bessie will lose the current round regardless of what card she plays
+// Remember to handle the cases where find(x) > 2*N, this means that Bessie will lose the current round regardless of what card she plays
 
 #include <bits/stdc++.h>
 using namespace std;
