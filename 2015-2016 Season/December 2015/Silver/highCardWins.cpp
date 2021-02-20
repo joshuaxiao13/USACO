498ee9adb57710a2c0eeb110a8fcdfb230236a1c
Joshua Xiao <66401883+joshuaxiao13@users.noreply.github.com>
2021-02-20 16:02:15 -0500

    Create highCardWins.cpp

---
+++ 2015-2016 Season/December 2015/Silver/highCardWins.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pb push_back
+#define eb emplace_back
+#define f first
+#define s second
+
+using ll = long long;
+using vi = vector<int>;
+
+const int mxN = 1e5;
+
+int N;
+int root[mxN + 2];
+
+void init(int N) {
+	for(int i = 1; i <= 2*N + 1; ++i) {
+		root[i] = i;
+	} 
+}
+
+int find(int u) {
+	if(root[u] == u) return u;
+	return root[u] = find(root[u]);
+}
+
+void unite(int u, int v) {
+	u = find(u);
+	v = find(v);
+	root[u] = v;
+}
+
+int main() {
+	
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+	
+	ifstream cin ("highcard.in");
+	ofstream cout ("highcard.out");
+	
+	cin >> N;
+	
+	vi elsie(N);
+	for(int&x : elsie)
+		cin >> x;
+	
+	sort(elsie.begin(), elsie.end());
+	
+	init(N);
+	
+	for(int i = 0; i < N; ++i)
+		unite(elsie[i], elsie[i] + 1);
+	
+	int ans = 0;
+	
+	for(int i = 0; i < N; ++i) {
+		int a = find(elsie[i]);
+		if(a > 2*N)
+			a = find(1);
+		else 
+			++ans;
+		unite(a, a + 1);
+	}
+	
+	cout << ans << '\n';
+	
+	return 0;
+}
