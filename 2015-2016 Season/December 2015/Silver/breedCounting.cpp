a43626bb37181a5e211e5393371bdcec6643d63e
Joshua Xiao <66401883+joshuaxiao13@users.noreply.github.com>
2021-02-20 20:09:05 -0500

    Create breedCounting.cpp

---
+++ 2015-2016 Season/December 2015/Silver/breedCounting.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pb push_back
+#define eb emplace_back
+#define f first
+#define s second
+
+using ll = long long;
+using vi = vector<int>;
+
+const int mxN = 1e5;
+int breed[3][mxN + 1];
+
+int main() {
+	
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+	
+	ifstream cin ("bcount.in");
+	ofstream cout ("bcount.out");
+	
+	int N, Q;
+	cin >> N >> Q;
+	
+	for(int i = 1; i <= N; ++i) {
+		int x;
+		cin >> x, --x;
+		++breed[x][i];
+		for(int j = 0; j < 3; ++j)
+			breed[j][i] += breed[j][i-1];
+	}
+	
+	while(Q--) {
+		int a, b;
+		cin >> a >> b;
+		for(int i = 0; i < 3; ++i) {
+			cout << breed[i][b] - breed[i][a-1] << (i == 2 ? "\n" : " ");
+		}
+	}
+	
+	return 0;
+}
