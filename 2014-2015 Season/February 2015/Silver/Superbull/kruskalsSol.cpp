b4eb6670b04ab49c9ee70ab45bbb85f9b9bc0404
Joshua Xiao <66401883+joshuaxiao13@users.noreply.github.com>
2021-02-24 18:00:48 -0500

    Create kruskalsSol.cpp

---
+++ 2014-2015 Season/February 2015/Silver/Superbull/kruskalsSol.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pb push_back
+#define eb emplace_back
+#define f first
+#define s second
+
+using ll = long long;
+using vi = vector<int>;
+
+int N;
+int root[2000], depth[2000], ID[2000];
+
+int find(int u) {
+	if(root[u] == u) return u;
+	return root[u] = find(root[u]);
+}
+
+bool same(int u, int v) {
+	return find(u) == find(v);
+}
+
+void unite(int u, int v) {
+	u = find(u);
+	v = find(v);
+	if(depth[u] < depth[v]) swap(u, v);
+	root[v] = u;
+	depth[u] = max(depth[u], depth[v] + 1);
+}
+
+int main() {
+	
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+	
+	ifstream cin ("superbull.in");
+	ofstream cout ("superbull.out");
+	
+	cin >> N;
+	
+	for(int i = 0; i < N; ++i) {
+		cin >> ID[i];
+		root[i] = i;
+	}
+	
+	vector<tuple<int,int,int>> edges;
+	for(int i = 0; i < N; ++i) {
+		for(int j = i + 1; j < N; ++j) {
+			edges.eb(ID[i] ^ ID[j], i, j);
+		}
+	}
+	
+	ll ans = 0;
+	sort(edges.rbegin(), edges.rend());
+
+	for(auto [w, u, v] : edges) {
+		if(!same(u, v)) {
+			unite(u, v);
+			--N;
+			ans += w;
+		}
+		if(N == 1)
+			break;
+	}
+	
+	cout << ans << '\n';
+	
+	return 0;
+}
