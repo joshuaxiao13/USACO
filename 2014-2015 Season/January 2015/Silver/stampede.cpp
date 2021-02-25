f0454b8040971758a16282dd5f4314aa06ec6065
Joshua Xiao <66401883+joshuaxiao13@users.noreply.github.com>
2021-02-24 23:20:33 -0500

    Create stampede.cpp

---
+++ 2014-2015 Season/January 2015/Silver/stampede.cpp
@@ -0,0 +1,108 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pb push_back
+#define eb emplace_back
+#define f first
+#define s second
+
+using ll = long long;
+using vi = vector<int>;
+
+struct endpoint {
+	int x, y, id;
+	bool enter;
+	bool operator< (const endpoint &p) {
+		if(x == p.x) {
+			if(enter && p.enter) return y < p.y;
+			else if(!enter && !p.enter) return y > p.y;
+			return enter;
+		}
+		return x < p.x;
+	}
+};
+
+int main() {
+	
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+	
+	ifstream cin ("stampede.in");
+	ofstream cout ("stampede.out");
+
+	int N;
+	cin >> N;
+	
+	// sweep line with endpoints representing the time each cow cross and exits line x = 0
+	
+	vector<endpoint> sweep;
+	
+	for(int i = 0; i < N; ++i) {
+		int x, y, r;
+		cin >> x >> y >> r;
+		x *= -1;
+		endpoint L, R;
+		L.x = (x - 1) * r;
+		R.x = x * r;
+		R.y = L.y = y;
+		L.enter = true;
+		R.enter = false;
+		R.id = L.id = i;
+		sweep.pb(L);
+		sweep.pb(R);
+	}
+	
+	sort(sweep.begin(), sweep.end());
+	
+	int ans = 0;
+	set<pair<int,int>> s;
+	vector<bool> spotted(N, false);		// spotted[id] stores whether cow with id has already been spotted by Farmer John 
+	
+	/*	There are 3 cases where a cow can be spotted by Farmer John:
+	 
+	  1) Cow segment crosses line x = 0, and has the least y-coordinate of all cow segments currently having some part on x = 0
+	  2) Cow segment exits line x = 0, and has the least y-coordinate of all cow segments currently having some part on x = 0
+	  3) Cow segment gets spotted when a Cow segment exits. There are 2 ways to check this:
+	  		a) let the Cow segment exit, then check whether the Cow segment with the least y-coordinate in the set has previously been marked
+	   		b) before a new Cow segment crosses line x = 0, check if the Cow segment  with the least y-coordinate in the set has previously been marked
+	  
+	  	which ever way you decide, you can paste this block of code in the appropiate place:
+	  
+	  	if(!s.empty()) {
+			int idx = (*s.rbegin()).s;
+			if(!spotted[idx]) {
+				++ans;
+				spotted[idx] = true;
+			}
+		}
+		
+	 */
+	
+	for(int i = 0; i < 2*N; ++i) {
+		endpoint &p = sweep[i];
+		if(p.enter) {
+			// paste 
+			s.insert({-p.y, p.id});
+			if((*s.rbegin()).f == -p.y) {
+				++ans;
+				spotted[p.id] = true;
+			}
+		}
+		else {
+			if((*s.rbegin()).f == -p.y && !spotted[p.id])
+				++ans;
+			s.erase({-p.y, p.id});
+			if(!s.empty()) {
+				int idx = (*s.rbegin()).s;
+				if(!spotted[idx]) {
+					++ans;
+					spotted[idx] = true;
+				}
+			}
+		}
+	}
+	
+	cout << ans << '\n';
+	
+	return 0;
+}
