d97fdc93c0f0b1bbcdb78275fb8110accd57880e
Joshua Xiao <66401883+joshuaxiao13@users.noreply.github.com>
2021-02-27 23:19:17 -0500

    Create justStalling.cpp

---
+++ 2020-2021 Season/January/Bronze/justStalling.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int N, a[20], b[20];
+
+int main() {
+	
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	cin >> N;
+	
+	for(int i = 0; i < N; ++i)
+		cin >> a[i];
+	
+	for(int i = 0; i < N; ++i)
+		cin >> b[i];
+	
+	sort(a, a + N, greater<int>());
+	sort(b, b + N, greater<int>());
+
+	long long ans = 1;
+	int bound = -1;
+
+	for(int i = 0; i < N; ++i) {
+		while(bound + 1 < N && b[bound + 1] >= a[i]) ++bound;
+		if(bound + 1 - i <= 0) {
+			cout << "0\n";
+			break;
+		}
+		ans *= bound + 1 - i;
+	}
+	
+	cout << ans << '\n';
+	
+	return 0;
+}
