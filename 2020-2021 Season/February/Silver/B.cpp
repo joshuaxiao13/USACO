7223443d9173305e6b8a3afd3660a6f49044b6fc
Joshua Xiao <66401883+joshuaxiao13@users.noreply.github.com>
2021-03-05 11:12:12 -0500

    Update B.cpp

--- 2020-2021 Season/February/Silver/B.cpp
+++ 2020-2021 Season/February/Silver/B.cpp
@@ -18,32 +18,28 @@ int main() {
 	cin >> N >> K;
 	
 	set<pair<int,int>> s;
-	vector<pair<int,int>> interval;
 	
 	for(int i = 0; i < N; ++i) {
 		int years;
 		cin >> years;
 		int left = (years/12);
 		int right = left + 1;
-		if(s.count(make_pair(left, right)) == 0) {
-			interval.eb(left, right);
-			s.insert({left, right});
-		}
+		s.insert({left, right});
 	}
 	
-	sort(interval.begin(), interval.end());
-	int n = interval.size();
-	int ans = 12*interval[n-1].s;
+	int ans = 12*(*s.rbegin()).s;
 	
 	vi gap;
-	gap.pb(interval[0].f);
-	for(int i = 1; i < n; ++i) {
-		gap.pb(interval[i].f - interval[i-1].s);
+	int prev = 0;
+	while(!s.empty()) {
+		gap.pb((*s.begin()).f - prev);
+		prev = (*s.begin()).s;
+		s.erase(s.begin());
 	}
 	
 	sort(gap.rbegin(), gap.rend());
 	
-	for(int i = 0; i < gap.size() && K > 1; ++i){
+	for(int i = 0; i < N - 1 && K > 1; ++i){
 		ans -= gap[i]*12;
 		--K;
 	}
